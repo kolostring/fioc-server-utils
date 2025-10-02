dd42dbc95d6924ce4cdb1a74bc1ee6fc5eee55ec
kolostring <yoerodriguezjunior@gmail.com>
2025-10-02 04:01:58 -0400

    feat: update to fioc 1.4.0

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,4 @@
-import { createDIToken, DIConsumer, DIContainer, DIToken } from "fioc";
+import { createDIToken, DIContainer, DIFactory, DIToken } from "fioc";
 
 /**
  * Builds a function that will resolve the provided tokens in a server container.
@@ -7,10 +7,17 @@ import { createDIToken, DIConsumer, DIContainer, DIToken } from "fioc";
  *
  * @param serverContainer
  * @returns a function that will resolve the provided token in the server container
+ * @example
+ * ```ts
+ * "use server"; //Don't forget to add this line
+ * import { serverContainer } from "@/ioc/serverContainer"; //Import the container that must run in the server
+ *
+ * export const iocServerHandler = buildIOCServerHandler(serverContainer); //Export the server action
+ * ```
  */
-export function buildIOCServerHandler(serverContainer: DIContainer) {
+export function buildIOCServerHandler<T>(serverContainer: DIContainer<T>) {
   return async (tokenKey: string, ...params: any[]) => {
-    const token = createDIToken(tokenKey);
+    const token = createDIToken().as(tokenKey);
 
     const resolved = serverContainer.resolve(token) as (
       ...params: unknown[]
@@ -30,20 +37,33 @@ export function buildIOCServerHandler(serverContainer: DIContainer) {
  */
 export const IOCServerHandlerToken = createDIToken<
   ReturnType<typeof buildIOCServerHandler>
->("IOCServerHandlerToken");
+>().as("IOCServerHandlerToken");
 
 /**
- * Creates a DIConsumer that will proxy the provided token in the server.
+ * Creates a DIFactory that will allow to run the provided controller in the server.
+ * When resolved in the client, returns a function with the same parameters and return type as the
+ * controller, so its easy to migrate from client to server and vice versa.
  *
  * @param token The token of the controller you want to proxy
- * @returns A DIConsumer that will proxy the controller in the server
+ * @returns A DIFactory that will proxy the controller in the server
+ * @example
+ * ```ts
+ * import { buildDIContainer } from "fioc";
+ * import { IOCServerHandlerToken, serverFactoryProxy } from "fioc-server-utils";
+ * import { iocServerHandler } from "@/ioc/iocServerHandler"; //Import the server action for ioc server handling
+ * import { ControllerCreateUserToken } from "./controllers/createUser"; //Import the controller Token(only) you want to proxy
+ *
+ * const clientContainer = buildDIContainer()
+ *   .register(IOCServerHandlerToken, iocServerHandler) //Register the server action
+ *   .registerConsumer(serverFactoryProxy(ControllerCreateUserToken)) //Register the proxy
+ *   .getResult(); //Get the result of the container
+ * ```
  */
-export function serverConsumerProxy<T extends DIToken<any>>(
-  token: T
-): DIConsumer<
-  [typeof IOCServerHandlerToken],
-  (args: unknown[]) => Promise<any>
-> {
+export function serverFactoryProxy<
+  T extends DIToken<Val, Key>,
+  Val,
+  Key extends string
+>(token: T): DIFactory<Key, [ReturnType<typeof buildIOCServerHandler>]> {
   return {
     token,
     dependencies: [IOCServerHandlerToken],
